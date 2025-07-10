0457d2633e60dce4bdddd498e094e24bc91b96f9
mcurmi05 <mcurmi05@gmail.com>
2025-07-10 18:31:54 +1000

    API keys to env oops

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "cf7092b7692902dee0b8d630008b8f80";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
