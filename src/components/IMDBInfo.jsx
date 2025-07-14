ee09123fcd68b4085642d663de1ea841369d74e4
mcurmi05 <mcurmi05@gmail.com>
2025-07-14 17:26:27 +1000

    better images

--- src/components/IMDBInfo.jsx
+++ src/components/IMDBInfo.jsx
@@ -1,26 +1,27 @@
-function IMDBInfo({movie}){
+function IMDBInfo({ movie }) {
+  const formatVotes = (votes) => {
+    if (!votes) return "0";
 
-    const formatVotes = (votes) => {
-        if (!votes) return "0";
+    if (votes >= 1000000) {
+      return "(" + (votes / 1000000).toFixed(1) + "M)";
+    } else if (votes >= 1000) {
+      return "(" + (votes / 1000).toFixed(0) + "K)";
+    } else {
+      return "(" + votes.toString() + ")";
+    }
+  };
 
-        if (votes >= 1000000) {
-            return "(" + (votes / 1000000).toFixed(1) + "M)";
-        } else if (votes >= 1000) {
-            return "(" + (votes / 1000).toFixed(0) + "K)";
-        } else {
-            return "(" + votes.toString() + ")";
-        }
-    };
-
-    return(
-        <a href={movie.url} target="_blank" className="imdb-rating">
-        <img src="/imdbicon.png" className="imdb-movie-card " />
-        <img src="/staricon.png" className="star-movie-card" />
-        <p>
-            {movie.averageRating ? movie.averageRating.toFixed(1) : "No ratings yet"} {movie.numVotes ? formatVotes(movie.numVotes): null} 
-        </p>
-        </a>
-    );
-    
+  return (
+    <a href={movie.url} target="_blank" className="imdb-rating">
+      <img src="/imdbicon.png" className="imdb-movie-card " />
+      <img src="/staricon.png" className="star-movie-card" />
+      <p>
+        {movie.averageRating
+          ? movie.averageRating.toFixed(1)
+          : "No ratings yet"}{" "}
+        {movie.numVotes ? formatVotes(movie.numVotes) : null}
+      </p>
+    </a>
+  );
 }
 export default IMDBInfo;
