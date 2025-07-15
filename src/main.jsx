381299fe4fd606e707bb6630ce41848cc3341e14
mcurmi05 <mcurmi05@gmail.com>
2025-07-15 18:04:58 +1000

    vercel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 createRoot(document.getElementById("root")).render(
   
   <BrowserRouter>
     <App />
+    <SpeedInsights />
   </BrowserRouter>
   
 );
