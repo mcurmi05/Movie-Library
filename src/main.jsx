ea3804cf9965db34d3532f3ce38db44161e48d4f
mcurmi05 <mcurmi05@gmail.com>
2025-07-17 00:24:09 +1000

    added vercel analytics component

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App.jsx";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react"
+
 
 createRoot(document.getElementById("root")).render(
   
   <BrowserRouter>
     <App />
     <SpeedInsights />
+    <Analytics/>
   </BrowserRouter>
   
 );
