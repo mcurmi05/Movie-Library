ef9de313bacc8c66578d4c6a528e492d02f8922d
mcurmi05 <mcurmi05@gmail.com>
2025-07-18 18:36:01 +1000

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['chunk-3HWLUFA5', 'chunk-JSO3YDVX']
+  }
 })
+
