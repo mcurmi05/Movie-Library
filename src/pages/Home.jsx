aff5c7902003dddf54f580035a6e6566106ceede
mcurmi05 <mcurmi05@gmail.com>
2025-07-18 16:55:11 +1000

    yep

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,12 @@ export default function Home(){
         <>
             <h2 style={{margin:"0 auto"}}>Home page in progress!!</h2>
             <img
-                src="/work-in-progress.png"
+                src="/work-in-progress.jpg"
                 style={{
                     margin: "0 auto",
                     marginTop: "10px",
+                    borderRadius:"50%",
+                    width:"250px"
                 }}
             ></img>       
 
