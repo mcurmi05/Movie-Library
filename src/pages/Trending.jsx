fa6cce8de7fd37483cb999d6f7a84ff6c517fa56
mcurmi05 <mcurmi05@gmail.com>
2025-07-18 16:29:36 +1000

    changed trending title

--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -48,7 +48,7 @@ function Trending() {
 
   return (
     <div className="trending">
-      <h1 style={{alignSelf:"center", marginTop:"-20px"}}>Top 100 Trending</h1>
+      <h1 style={{alignSelf:"center", marginTop:"-20px"}}>Top 100 Trending Movies</h1>
       {error && <div className="error-message">{error}</div>}
 
       {loading ? (
