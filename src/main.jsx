c7c947b1c277f6fbe7bfc8a0389ddfc753b67ed6
mcurmi05 <mcurmi05@gmail.com>
2025-07-19 19:55:23 +1000

    oops forgot import

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import "./styles/index.css";
 import App from "./App.jsx";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react"
+import React from "react";
+
 
 
 createRoot(document.getElementById("root")).render(
