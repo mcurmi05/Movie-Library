11a794d48093d68b6fad70f6251ee27bce2b0a0d
mcurmi05 <mcurmi05@gmail.com>
2025-07-24 01:31:39 +1000

    added placeholder for no cast image

--- src/components/CastList.jsx
+++ src/components/CastList.jsx
@@ -8,7 +8,7 @@ function CastList({ movie }) {
         
         {movie.cast.filter(castMember => castMember.job === "actor" || castMember.job === "actress").map((castMember, index) => (
             <div className="cast-member-container" key={index}>
-                <img src={castMember.primaryImage} className="cast-image"/>
+                <img src={castMember.primaryImage ? `${castMember.primaryImage}`: "/placeholderimage.jpg"} className="cast-image"/>
                 <p className="cast-member-fullname">{castMember.fullName}</p>
                 <p className="cast-member-characters">{castMember.characters && castMember.characters.length > 0 ? castMember.characters.join(", ") : ""}</p>
             </div>
